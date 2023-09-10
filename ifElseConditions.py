45b27bf502fe336ab5175bb16aef87eb49c4d825
Moksh Yadav <131985331+MokshYadav007@users.noreply.github.com>
2023-09-10 16:09:39 +0530

    Create ifElseConditions.py

---
+++ ifElseConditions.py
@@ -0,0 +1,14 @@
+print("What is your age")
+age = int(input())
+if age<18:
+    print("You cannot drive")
+
+#if age>105:
+   # print("Are you kidding")
+
+elif age==18:
+    print("We will think about you")
+
+else:
+    print("You can drive")
+
