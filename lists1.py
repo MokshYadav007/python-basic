f2a66c103395ce458d68e44cde4f4c1770349803
Moksh Yadav <131985331+MokshYadav007@users.noreply.github.com>
2023-09-13 19:37:06 +0530

    lists1.py

---
+++ lists1.py
@@ -0,0 +1,16 @@
+#numbers = [5, 45, 6, 32, 2, 4]
+#numbers.sort()
+#numbers.reverse()
+#numbers.append(8)
+#numbers.insert(3, 9)
+#print(numbers)
+a = 3
+b = 5
+#METHOD 1
+#a, b = b, a
+#print(a,b)
+#there another method for other progamming languages
+temp = a
+a = b
+b = temp
+print(a, b)
