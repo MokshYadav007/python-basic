c3f82f7711d5500b2cb3a6604a6a84fef8c0d622
Moksh Yadav <131985331+MokshYadav007@users.noreply.github.com>
2023-09-20 19:56:59 +0530

    Create ExceptionHandling.py

---
+++ ExceptionHandling.py
@@ -0,0 +1,16 @@
+f1 = open("jesse.txt")
+
+try:
+    f = open("does.txt")
+
+except Exception as e:
+    print(e)
+
+else:
+    print("this will only run if except")
+finally:
+    print("run this anyway")
+    #f.close()
+    f1.close()
+
+print("Important stuff")
