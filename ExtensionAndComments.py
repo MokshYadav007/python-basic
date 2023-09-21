f1bcf9db472f83d427dad58e488b334720d98adf
Moksh Yadav <131985331+MokshYadav007@users.noreply.github.com>
2023-09-21 18:58:42 +0530

    Create ExtensionAndComments.py

---
+++ ExtensionAndComments.py
@@ -0,0 +1,10 @@
+#please dont remove thiis line
+"""this
+is
+a
+comment
+"""
+#print("my name is","chikka chikka")
+#print("slim shady")
+#print("do you like violence")
+print("slim shady is \n eminem \t really")
