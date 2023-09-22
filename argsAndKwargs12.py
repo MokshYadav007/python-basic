948a309731420e1de5733da0c6fe9fd45c6c58b6
Moksh Yadav <131985331+MokshYadav007@users.noreply.github.com>
2023-09-22 21:42:12 +0530

    argsAndKwargs12.py

--- argsAndKwargs12.py
+++ argsAndKwargs12.py
@@ -7,7 +7,7 @@ def funargs(normal, *argsmoksh, **kwargs):
         print(item)
     print("\nNow i would like to introduce some of their occupation")
     for key, value in kwargs.items():
-        print(f"{key} is  {value}")
+        print(f"{key} is  {value}") 
 # function_name_print("Harry", "Bruce", "Vishal", "Skillf", "Daryl")
 
 har = ["Harry", "Bruce", "Vishal", "Skillf", "Daryl"]
