730e15780f476e8fd37d9321a44b35c41439ca59
Moksh Yadav <131985331+MokshYadav007@users.noreply.github.com>
2023-09-24 15:41:22 +0530

    Create DemoDictionary.py

---
+++ DemoDictionary.py
@@ -0,0 +1,13 @@
+d1 = {}
+#print(type(d1))
+d2 = {"Batman":"Joker", "Superman":"ZOD", "Flash":"Reverse Flash","Lucifer":"GOD" }
+#print(d2["Batman"])
+#for adding extra
+d2["Nightwing"] = "Deathshot"
+d2["Constantine"] = "Anyone"
+#for deleting
+del d2["Nightwing"]
+#print(d2.get("Batman"))
+#d2.update({"SpiderMan":"Goblin"})
+#print(d2.keys())
+print(d2.items())
